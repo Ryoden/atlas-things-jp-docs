362fd94993a910513c58fb9b45aebe781dbd13f9
tooruuetani <tooru.uetani@mgw.ryoden.co.jp>
2018-12-16 01:30:36 +0900

    styled-components でスタイル記述

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,34 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+
+const HeaderBar = styled.div`
+  background-color: #1eb9ee;
+  margin-bottom: 1.45rem;
+`
+
+const HeaderBody = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: '#1eb9ee',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderBar>
+    <HeaderBody>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+        <StyledLink to="/">
           {siteTitle}
-        </Link>
+        </StyledLink>
       </h1>
-    </div>
-  </div>
+    </HeaderBody>
+  </HeaderBar>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+
+const LogoPanel = styled.div`
+  max-width: 300px;
+  margin-bottom: 1.45rem;
+`
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <LogoPanel>
       <Image />
-    </div>
+    </LogoPanel>
   </Layout>
 )
 
