b42e4afe9587a541e517c0dc3ad6084a8b36ee90
toruuetani <tooru.uetani@mgw.ryoden.co.jp>
2018-12-17 17:56:23 +0900

    価格表更新

--- src/components/price.js
+++ src/components/price.js
@@ -10,115 +10,63 @@ const Hero = styled.section.attrs({
   background-position: center center;
 `
 
-const PriceRoot = styled.div.attrs({
-  className: 'columns'
-})`
-    margin-left: auto;
-    margin-right: auto;
-`
+function PriceItem({name, quantity, price}) {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td className="has-text-right">{quantity}</td>
+      <td className="has-text-right">{price}</td>
+    </tr>
+  )
+}
 
-const PriceItem = styled.div.attrs({
-  className: 'column card'
-})`
-  margin-top: 50px;
-  margin-left: 30px;
-  margin-right: 30px;
-  margin-bottom: 70px;
-`
-
-const PriceItemHeader = styled.header.attrs({
-  className: 'card-header is-size-5 has-text-weight-bold'
-})``
-
-const PriceItemContent = styled.div.attrs({
-  className: 'card-content is-size-6'
-})``
+function PriceList({name, children}) {
+  return (
+    <div className="column">
+      <div className="card">
+        <header className="card-header">
+          <div className="card-header-title">{name}</div>
+        </header>
+        <div className="card-content">
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th className="has-text-centered" style={{minWidth:"10em"}}>月間最大入出荷数</th>
+                <th className="has-text-centered">月額</th>
+              </tr>
+            </thead>
+            <tbody>
+              {children}
+            </tbody>
+          </table>
+        </div>
+        <footer className="card-footer"></footer>
+      </div>
+    </div>
+  )
+}
 
 const Price = () => (
   <Hero id="price">
-    <TitleBar title="価格表" subtitle="（価格は税抜きです。）" />
-    <PriceRoot className="hero-body">
-      <PriceItem>
-        <PriceItemHeader>Starter</PriceItemHeader>
-        <PriceItemContent>
-          <table className="table">
-            <thead>
-              <tr><th colSpan="2">月間最大入出荷数</th></tr>
-            </thead>
-            <tbody>
-              <tr><td>Small</td><td>20,000</td></tr>
-              <tr><td>Medium</td><td>50,000</td></tr>
-              <tr><td>Large</td><td>100,000</td></tr>
-            </tbody>
-          </table>
-          <table className="table">
-            <thead>
-              <tr><th colSpan="2">月額</th></tr>
-            </thead>
-            <tbody>
-              <tr><td>Small</td><td>￥50,000</td></tr>
-              <tr><td>Medium</td><td>￥122,000</td></tr>
-              <tr><td>Large</td><td>￥234,000</td></tr>
-            </tbody>
-          </table>
-        </PriceItemContent>
-      </PriceItem>
-      <PriceItem>
-        <PriceItemHeader>Professioanl</PriceItemHeader>
-        <PriceItemContent>
-        <table className="table">
-            <thead>
-              <tr><th colSpan="2">月間最大入出荷数</th></tr>
-            </thead>
-            <tbody>
-              <tr><td>Small</td><td>250,000</td></tr>
-              <tr><td>Medium</td><td>500,000</td></tr>
-              <tr><td>Large</td><td>750,000</td></tr>
-            </tbody>
-          </table>
-          <table className="table">
-            <thead>
-              <tr><th colSpan="2">月額</th></tr>
-            </thead>
-            <tbody>
-              <tr><td>Small</td><td>￥420,000</td></tr>
-              <tr><td>Medium</td><td>￥546,000</td></tr>
-              <tr><td>Large</td><td>￥630,000</td></tr>
-            </tbody>
-          </table>
-        </PriceItemContent>
-      </PriceItem>
-      <PriceItem>
-        <PriceItemHeader>Enterprise</PriceItemHeader>
-        <PriceItemContent>
-        <table className="table">
-            <thead>
-              <tr><th colSpan="2">月間最大入出荷数</th></tr>
-            </thead>
-            <tbody>
-              <tr><td>Small</td><td>1,000,000</td></tr>
-              <tr><td>Medium</td><td>3,000,000</td></tr>
-              <tr><td>Large</td><td>5,000,000</td></tr>
-            </tbody>
-          </table>
-          <table className="table">
-            <thead>
-              <tr><th colSpan="2">月額</th></tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Small</td>
-                <td rowSpan="3" style={{ verticalAlign: "middle", textAlign: "center" }}>
-                  お問い合わせ<br />ください
-                </td>
-              </tr>
-              <tr><td>Medium</td></tr>
-              <tr><td>Large</td></tr>
-            </tbody>
-          </table>
-        </PriceItemContent>
-      </PriceItem>
-    </PriceRoot>
+    <TitleBar title="価格表（価格はすべて税抜きです）" />
+    <div className="hero-body columns">
+      <PriceList name="Starter">
+        <PriceItem name="Small" quantity="20,000" price="￥50,000" />
+        <PriceItem name="Medium" quantity="50,000" price="￥122,000" />
+        <PriceItem name="Large" quantity="100,000" price="￥234,000" />
+      </PriceList>
+      <PriceList name="Professional">
+        <PriceItem name="Small" quantity="250,000" price="￥420,000" />
+        <PriceItem name="Medium" quantity="500,000" price="￥546,000" />
+        <PriceItem name="Large" quantity="750,000" price="￥630,000" />
+      </PriceList>
+      <PriceList name="Enterprise">
+        <PriceItem name="Small" quantity="1,000,000" price="" />
+        <PriceItem name="Medium" quantity="2,000,000" price="" />
+        <PriceItem name="Large" quantity="3,000,000" price="" />
+      </PriceList>
+    </div>
   </Hero>
 )
 
