fbac58cfd9cbfcebc5e5b10261e594d25e8d92a7
tooruuetani <tooru.uetani@mgw.ryoden.co.jp>
2018-12-22 01:24:36 +0900

    navbar のトグル実装

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,44 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 
 import Logo from '../images/logo.png'
 
+class Header extends React.Component {
+  state = {
+    isActive: false,
+  }
 
-const Header = ({ siteTitle }) => (
-  <div className="navbar is-light is-fixed-top" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item" href="/#">
-        <img src={Logo} alt={siteTitle} width="112" height="28" />
-      </a>
-    </div>
+  toggleNav = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive
+    }))
+  }
 
-    <div className="mabvar-menu" id="nav">
-      <div className="navbar-start"></div>
-      <div className="navbar-end">
-        <a className="navbar-item is-tab" href="/#">TOP</a>
-        <a className="navbar-item is-tab" href="/#feature">特徴</a>
-        <a className="navbar-item is-tab" href="/#price">価格表</a>
-        <a className="navbar-item is-tab" href="/#contact">お問い合わせ</a>
+  render(siteTitle) {
+    return (
+      <div className="navbar is-light is-fixed-top" role="navigation" aria-label="dropdown navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/#">
+            <img src={Logo} alt={siteTitle} width="112" height="28" />
+          </a>
+          <div className="navbar-burger" data-target="navMenu" onClick={this.toggleNav}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div className={ this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'} id="navMenu">
+          <div className="navbar-end" onClick={this.toggleNav}>
+            <a className="navbar-item is-tab" href="/#">TOP</a>
+            <a className="navbar-item is-tab" href="/#feature">特徴</a>
+            <a className="navbar-item is-tab" href="/#price">価格表</a>
+            <a className="navbar-item is-tab" href="/#contact">お問い合わせ</a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: '',
+    )
+  }
 }
 
 export default Header
